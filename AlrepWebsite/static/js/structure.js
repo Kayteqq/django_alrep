cd461ee0271f67af83c49170570c13d1ffe2e852
Kayteq <kajetan.j.nowakowski@gmail.com>
2026-05-28 08:14:54 +0200

    loading fix

--- AlrepWebsite/static/js/structure.js
+++ AlrepWebsite/static/js/structure.js
@@ -1,24 +1,6 @@
-const header = document.querySelector('.header');
-const headerImages = header.querySelectorAll('img');
-
-let loadedCount = 0;
-const totalImages = headerImages.length;
-
-function onAllImagesLoaded() {
-  header.classList.add('is-loaded');
-}
-
-function checkImage() {
-  loadedCount++;
-  if (loadedCount === totalImages) {
-    onAllImagesLoaded();
-  }
-}
-headerImages.forEach(img => {
-  if (img.complete) {
-    checkImage();
-  } else {
-    img.addEventListener('load', checkImage);
-    img.addEventListener('error', checkImage); // Bezpiecznik dla uszkodzonych linków
+window.addEventListener('load', () => {
+  const header = document.querySelector('.main-header');
+  if (header) {
+    header.classList.add('is-loaded');
   }
 });
